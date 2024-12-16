44d22ffa8e53bff2cbf468f383f10dd6f2b7645e
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-16 15:07:16 -0300

    learning in module one

---
+++ variaveis.py
@@ -0,0 +1,4 @@
+"""Variáveis é oq definimos para guardar dados simples"""
+nomeCompleto = "Matheus Coutinho"# camel-case, é mais usada em Class no python 
+nome_completo = "Matheus Coutinho" # snake-case, é mais usado para variáveis, 
+#funções e métodos em python
