301e91177f03b8e85263e4d25c7abf5b22d3d9a6
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-16 19:14:14 -0300

    learning in module one

--- tipos_variaveis/numeros.py
+++ tipos_variaveis/numeros.py
@@ -6,6 +6,39 @@ print("Inteiro =", numero_inteiro)
 numero_real = 3.14
 print("Real =", numero_real)
 
-#type()
+# type()
 print("Tipo da variável inteiro", type(numero_inteiro))
 print("Tipo da variável flutuante", type(numero_real))
+
+# soma + 
+soma = 1 + 1
+print("1 + 1 = ", soma)
+
+# subtração - 
+subtracao = 1 - 1
+print("1 - 1 = ", subtracao)
+
+# multiplicação * 
+multiplicacao = 2 * 2
+print("2 * 2 = ", multiplicacao)
+
+# divisao / 
+divisao = 5 / 2
+print("5 / 2 = ", divisao)
+print("Tipo da variável do resultado da divisão", type(divisao))
+
+# int()
+conversao_divisao = int(divisao)
+print("Valor em inteiro = ", conversao_divisao, type(conversao_divisao))
+
+# float()
+print("Valor em número real = ", float(divisao))
+
+# Modulo %, é o restante da divisão, é oq fica sobrando da divisão 
+modulo = 7 % 3
+print("Modulo = ", modulo)
+
+# divisao inteira // 
+divisao_inteira = 5 // 2
+print("5 // 2 = ", divisao_inteira)
+print("Tipo da variável do resultado da divisão", type(divisao_inteira))
