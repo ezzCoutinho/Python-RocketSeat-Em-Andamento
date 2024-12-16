b60094861cc574aee1ff8b46a004b9204ac2be0b
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-16 15:37:44 -0300

    learning in module one

---
+++ tipos_variaveis/numeros.py
@@ -0,0 +1,11 @@
+# Inteiro
+numero_inteiro = 4
+print("Inteiro =", numero_inteiro)
+
+# Número real com ponto flutuante FLOATS
+numero_real = 3.14
+print("Real =", numero_real)
+
+#type()
+print("Tipo da variável inteiro", type(numero_inteiro))
+print("Tipo da variável flutuante", type(numero_real))
--- variaveis.py
+++ variaveis.py
@@ -2,3 +2,4 @@
 nomeCompleto = "Matheus Coutinho"# camel-case, é mais usada em Class no python 
 nome_completo = "Matheus Coutinho" # snake-case, é mais usado para variáveis, 
 #funções e métodos em python
+
