233b8ef23f1b35bba25bc81d3f50d844f9332e20
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-19 18:47:28 -0300

    learning in module one

---
+++ tipos_variaveis/lista.py
@@ -0,0 +1,14 @@
+# Declaração 
+minha_lista = [1, 2, 3, 4, 5, "rocketseat", True, False]
+
+# Exibindo a lista
+print("Minha lista como exemplo", minha_lista)
+
+# Exibindo índices da lista
+print("minha_lista[0], ", minha_lista[0])
+print("minha_lista[0], ", minha_lista[5])
+print("minha_lista[1:6], ", minha_lista[1:6])
+# Do começo até...
+print("minha_lista[:3]", minha_lista[:3])
+# Do alvo até...
+print("minha_lista[8:]", minha_lista[6:])
