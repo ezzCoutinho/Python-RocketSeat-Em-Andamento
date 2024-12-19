5e83c35b9ff78d5d5065fc3e807503961a0e060d
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-19 18:27:02 -0300

    learning in module one

---
+++ tipos_variaveis/booleanos.py
@@ -0,0 +1,29 @@
+# Condição verdadeira
+True
+
+# Condição falsa
+False
+
+# Condicionais if, else if e else
+if True: 
+  print("Bloco será executado")
+else: 
+  print("Bloco não será executado")
+
+# Operadores lógicos
+
+# And "e"
+
+if True and True: 
+  print("Bloco será executado")
+
+if True and False:
+  print("Bloco não será executado")
+
+# Or "ou"
+
+if True or False:
+  print("Bloco será executado")
+
+if False or False:
+  print("Este bloco não será executado")
