da4880fd4b4837356e91a8f40c3cbd284285bae2
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-20 15:05:31 -0300

    learning in module one

---
+++ while.py
@@ -0,0 +1,13 @@
+contador = 0
+
+while contador <  5:
+  print("Contagem:", contador)
+  contador += 1 
+
+while True:
+  print("Contagem:", contador)
+  contador += 1
+  if contador == 4:
+    break
+
+print("Programa finalizado.")
