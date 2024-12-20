d653c26018e441c6507bc39e31c881a9f124d0b1
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-20 10:16:46 -0300

    learning in module one

---
+++ tipos_variaveis/tupla.py
@@ -0,0 +1,11 @@
+# Tupla
+
+minha_tupla = (1, 2, 3, 4, 4, 5)
+
+print(minha_tupla)
+
+contagem_tupla = minha_tupla.count(4)
+print(contagem_tupla)
+
+indice_tupla = minha_tupla.index(3)
+print(indice_tupla)
