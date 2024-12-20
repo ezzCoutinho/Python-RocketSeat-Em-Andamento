2b498cf48a3eb9404522028013fb6f5d386c5946
ezzCoutinho <lammerlopes@hotmail.com>
2024-12-20 10:44:48 -0300

    learning in module one

---
+++ tipos_variaveis/dicionarios.py
@@ -0,0 +1,12 @@
+# Criando um dicionário NÃO ORDENADO, com pares de chave e valor
+
+pessoa = {"nome": "Gabriel Casemiro", "idade": 30, "cidade": "São Paulo"}
+
+
+# Acessando valores por chave
+print("Nome:", pessoa["nome"])
+print("Idade:", pessoa["idade"])
+print("Cidade:", pessoa["cidade"])
+
+pessoa["sobrenome"] = "Silva"
+print("Sobrenome:", pessoa["sobrenome"])
